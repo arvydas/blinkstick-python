60c8061ebabb381974080a925cbcdba27e40ac2d
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2013-04-20 14:59:54 +0100

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='BlinkStick',
-    version='0.3.0',
+    version='0.3.1',
     author='Arvydas Juskevicius',
     author_email='arvydas@arvydas.co.uk',
     packages=find_packages(),
